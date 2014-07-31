62d575d1ce55ce0161118f2a93b85ab5d6c95869
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-07-31 17:37:28 +0900

    リップルのシーン追加

---
+++ Assets/nobnak/Ripple/RippleSurface.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace nobnak.Ripple {
+
+	public class RippleSurface : MonoBehaviour {
+		public const string SHADER_PROP_RIPPLES = "_Ripples";
+
+		public Material rippleSurfaceMat;
+		public Material[] outputMats;
+		public int size = 256;
+		public int fps = 30;
+		public int maxRipples = 128;
+
+		private RenderTexture _rtex0, _rtex1;
+		private float _dt;
+		private float _tPrev = 0f;
+		private ComputeBuffer _ripplesBuffer;
+		private Ripple[] _ripples0 = new Ripple[0], _ripples1 = new Ripple[0];
+
+		void Start() {
+			_rtex0 = new RenderTexture(size, size, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
+			_rtex1 = new RenderTexture(size, size, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
+			_rtex0.wrapMode = TextureWrapMode.Repeat;
+			_rtex1.wrapMode = TextureWrapMode.Repeat;
+			RenderTexture.active = _rtex0;
+			GL.Clear(true, true, Color.black);
+			RenderTexture.active = _rtex1;
+			GL.Clear(true, true, Color.black);
+			RenderTexture.active = null;
+
+			_dt = 1f / fps;
+			_tPrev = Time.timeSinceLevelLoad;
+		}
+
+		void Update() {
+			var n = Mathf.FloorToInt((Time.timeSinceLevelLoad - _tPrev) / _dt);
+			_tPrev += n * _dt;
+
+			if (Input.GetMouseButton(0)) {
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (collider.Raycast(ray, out hit, float.MaxValue)) {
+					if (_ripples0.Length >= maxRipples) {
+						if (_ripples1.Length < maxRipples)
+							System.Array.Resize(ref _ripples1, maxRipples);
+						System.Array.Copy(_ripples0, 1, _ripples1, 0, maxRipples-1);
+					} else {
+						System.Array.Resize(ref _ripples1, _ripples0.Length + 1);
+						System.Array.Copy(_ripples0, _ripples1, _ripples0.Length);
+					}
+					_ripples1[_ripples1.Length - 1] = new Ripple(hit.textureCoord, Time.timeSinceLevelLoad);
+					var tmpRipples = _ripples0; _ripples0 = _ripples1; _ripples1 = tmpRipples;
+
+					if (_ripplesBuffer != null)
+						_ripplesBuffer.Release();
+					_ripplesBuffer = new ComputeBuffer(_ripples0.Length, Marshal.SizeOf(typeof(Ripple)));
+					_ripplesBuffer.SetData(_ripples0);
+					rippleSurfaceMat.SetBuffer(SHADER_PROP_RIPPLES, _ripplesBuffer);
+				}
+			}
+
+			for (var i = 0; i < n; i++) {
+				Graphics.Blit(_rtex0, _rtex1, rippleSurfaceMat);
+				foreach (var mat in outputMats)
+					mat.mainTexture = _rtex1;
+				var tmpRtex = _rtex0; _rtex0 = _rtex1; _rtex1 = tmpRtex;
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		struct Ripple {
+			public Vector2 center;
+			public float tStart;
+
+			public Ripple(Vector2 center, float tStart) {
+				this.center = center;
+				this.tStart = tStart;
+			}
+		}
+	}
+}
---
+++ Assets/nobnak/Ripple/RippleSurface.shader
@@ -0,0 +1,69 @@
+﻿Shader "Custom/RippleSurface" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Params ("Speed, Atten, Height, Width", Vector) = (0.1, 1.0, 1.0, 0.1)
+		_K ("Damping", Float) = 0.05
+	}
+	SubShader {
+		ZTest Always Cull Off ZWrite Off Fog { Mode Off }
+		
+		Pass {
+			CGPROGRAM
+			#pragma target 5.0
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			sampler2D _MainTex;
+			float4 _MainTex_TexelSize;
+			float4 _Params;
+			float _K;
+			
+			struct Ripple {
+				float2 center;
+				float tStart;
+			};
+			
+			StructuredBuffer<Ripple> _Ripples;
+			
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			struct vs2ps {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			
+			vs2ps vert(appdata IN) {
+				vs2ps OUT;
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
+				OUT.uv = IN.uv;
+				return OUT;
+			}
+			
+			float4 frag(vs2ps IN) : COLOR {
+				float h = tex2D(_MainTex, IN.uv).r;
+				h *= saturate(1.0 - _K);
+				
+				uint numStructs;
+				uint stride;
+				_Ripples.GetDimensions(numStructs, stride);
+				for (uint i = 0; i < numStructs; i++) {
+					Ripple ripple = _Ripples[i];
+					float t = _Time.y - ripple.tStart;
+					float rRipple = t * _Params.x;
+					float hRipple = _Params.z * exp(-_Params.y * rRipple);
+					float r = distance(ripple.center, IN.uv);
+					float dh = hRipple
+						* smoothstep(rRipple - _Params.w, rRipple, r) 
+						* smoothstep(rRipple + _Params.w, rRipple, r);
+					h += dh;
+				}
+				
+				return float4(h, 0.0, 0.0, 0.0);
+			}
+			ENDCG
+		}
+	} 
+}
