d24ae1fc64affe2017ccdccab1d52ec7ae47f038
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-11-03 00:43:35 +0900

    no message

---
+++ Assets/nobnak/Menbrane/Menbrane.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace nobnak.Menbrane {
+
+	public class Menbrane : MonoBehaviour {
+		public const string SHADER_DT = "_Dt";
+		public const string SHADER_DX = "_Dx";
+		public const string SHADER_T = "_T";
+
+		public Material sim;
+		public Material[] outputMats;
+		public Material brushMat;
+		public Texture2D brushTex;
+		public int nGrids = 256;
+		public float l = 4;
+		public int fps = 30;
+		public float tension = 3;
+		public float density = 10;
+
+		private RenderTexture _vhTex0, _vhTex1;
+		private float _dt;
+		private float _tPrev = 0f;
+
+		void Start() {
+			_vhTex0 = new RenderTexture(nGrids, nGrids, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
+			_vhTex1 = new RenderTexture(nGrids, nGrids, 0, RenderTextureFormat.RGFloat, RenderTextureReadWrite.Linear);
+			_vhTex0.wrapMode = _vhTex1.wrapMode = TextureWrapMode.Repeat;
+			_vhTex0.Create();
+			_vhTex1.Create();
+
+			RenderTexture.active = _vhTex0;
+			GL.Clear(true, true, Color.black);
+			RenderTexture.active = _vhTex1;
+			GL.Clear(true, true, Color.black);
+			RenderTexture.active = null;
+
+			_tPrev = Time.timeSinceLevelLoad;
+		}
+
+		void Update() {
+			var dx = l / nGrids;
+			var m = density * dx * dx;
+			_dt = 1f / fps;
+			var nSteps = Mathf.FloorToInt((Time.timeSinceLevelLoad - _tPrev) / _dt);
+			_tPrev += nSteps * _dt;
+
+			sim.SetFloat(SHADER_DX, dx);
+			sim.SetFloat(SHADER_DT, _dt);
+			sim.SetFloat(SHADER_T, tension / m);
+
+			if (Input.GetMouseButton(0)) {
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (collider.Raycast(ray, out hit, float.MaxValue)) {
+					var pxPos = nGrids * hit.textureCoord;
+					var projMat = Matrix4x4.Ortho(0f, nGrids, 0f, nGrids, -1f, 1f);
+					GL.PushMatrix();
+					GL.LoadIdentity();
+					GL.LoadProjectionMatrix(projMat);
+					RenderTexture.active = _vhTex0;
+					var paintRect = new Rect(pxPos.x - 0.5f * brushTex.width, pxPos.y - 0.5f * brushTex.height, brushTex.width, brushTex.height);
+					Graphics.DrawTexture(paintRect, brushTex, brushMat);
+					RenderTexture.active = null;
+					GL.PopMatrix();
+				}
+			}
+
+
+			for (var i = 0; i < nSteps; i++) {
+				Graphics.Blit(_vhTex0, _vhTex1, sim);
+				Swap();
+			}
+			foreach (var mat in outputMats) {
+				mat.mainTexture = _vhTex0;
+				mat.SetFloat(SHADER_DX, dx);
+			}
+		}
+		void Swap() {
+			var tmpRtex = _vhTex0; _vhTex0 = _vhTex1; _vhTex1 = tmpRtex;
+		}
+	}
+}
--- Assets/nobnak/Menbrane/NormalGen.shader
+++ Assets/nobnak/Menbrane/NormalGen.shader
@@ -1,7 +1,6 @@
-﻿Shader "Custom/HeightField" {
+﻿Shader "Custom/NormalGen" {
 	Properties {
 		_MainTex ("Base (RGB)", 2D) = "white" {}
-		_Gain ("Gain", Float) = 1.0
 		_Dx ("Delta X", Float) = 1
 	}
 	SubShader {
@@ -16,40 +15,31 @@
 			#include "UnityCG.cginc"
 			
 			float4 _MainTex_TexelSize;
-			
-			sampler2D _MainTex;
-			float _Gain;
-			float _Dx;
 
+			sampler2D _MainTex;
+			float _Dx;
+			
 			struct Input {
 				float4 vertex : POSITION;
 				float2 uv : TEXCOORD0;
 			};
-			struct vs2ps {
-				float4 vertex : POSITION;
-				float2 uv: TEXCOORD0;
-			};
 			
-			vs2ps vert(Input IN) {
-				float h = tex2Dlod(_MainTex, float4(IN.uv, 0.0, 0.0)).r;
-				float4 posProj = mul(UNITY_MATRIX_MVP, IN.vertex);
-				posProj.y += _Gain * h;
-				
-				vs2ps OUT;
-				OUT.vertex = posProj;
+			Input vert(Input IN) {
+				Input OUT;
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
 				OUT.uv = IN.uv;
 				return OUT;
 			}
 
-			float4 frag(vs2ps IN) : COLOR {
+			float4 frag(Input IN) : COLOR {
 				float2 dtex = _MainTex_TexelSize.xy;			
 				float hx0 = tex2D(_MainTex, IN.uv + float2(-0.5, 0) * dtex).r;
 				float hx1 = tex2D(_MainTex, IN.uv + float2(+0.5, 0) * dtex).r;
 				float hy0 = tex2D(_MainTex, IN.uv + float2(0, -0.5) * dtex).r;
 				float hy1 = tex2D(_MainTex, IN.uv + float2(0, +0.5) * dtex).r;
-								
-				float dhdx = _Gain * (hx1 - hx0) / _Dx;
-				float dhdy = _Gain * (hy1 - hy0) / _Dx;
+				
+				float dhdx = (hx1 - hx0) / _Dx;
+				float dhdy = (hy1 - hy0) / _Dx;
 				float3 n = normalize(float3(-dhdx, -dhdy, 1.0));
 				return float4(0.5 * (n + 1.0), 1.0);
 			}
